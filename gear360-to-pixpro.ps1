636e573fd18c21a482d5861c13c618d4a79d2449
Aub Guillemette <aubertguillemette@gmail.com>
2025-02-20 14:56:05 -0500

    Update gear360-to-pixpro.ps1

--- gear360-to-pixpro.ps1
+++ gear360-to-pixpro.ps1
@@ -1,41 +1,41 @@
 param (
-    [string]$input_file,
-    [string]$output_file,
-    [string]$metadata_dir_path = "metadata",
-    [string]$ffmpeg_path = (Get-Command ffmpeg -ErrorAction SilentlyContinue).Path,
-    [string]$mp4edit_path = (Get-Command mp4edit -ErrorAction SilentlyContinue).Path
+	[string]$input_file,
+	[string]$output_file,
+	[string]$metadata_dir_path = "metadata",
+	[string]$ffmpeg_path = (Get-Command ffmpeg -ErrorAction SilentlyContinue).Path,
+	[string]$mp4edit_path = (Get-Command mp4edit -ErrorAction SilentlyContinue).Path
 )
 
 if (-not $input_file) {
-    Write-Host "Usage: gear360-to-pixpro.ps1 -input_file <input_file> [-output_file <output_file>] [-metadata_dir_path <metadata_dir_path>] [-ffmpeg_path <ffmpeg_path>] [-mp4edit_path <mp4edit_path>]"
-    exit 1
+	Write-Host "Usage: gear360-to-orbit360.ps1 -input_file <input_file> [-output_file <output_file>] [-metadata_dir_path <metadata_dir_path>] [-ffmpeg_path <ffmpeg_path>] [-mp4edit_path <mp4edit_path>]"
+	exit 1
 }
 
 if (-not (Test-Path -Path $metadata_dir_path -PathType Container)) {
-    Write-Host "Metadata directory does not exist: $metadata_dir_path"
-    exit 1
+	Write-Host "Metadata directory does not exist: $metadata_dir_path"
+	exit 1
 }
 
 if (-not $ffmpeg_path) {
-    Write-Host "ffmpeg not found. (You can set the ffmpeg_path parameter to specify the path to ffmpeg)"
-    exit 1
+	Write-Host "ffmpeg not found. (You can set the ffmpeg_path parameter to specify the path to ffmpeg)"
+	exit 1
 }
 if (-not $mp4edit_path) {
-    Write-Host "mp4edit not found. (You can set the mp4edit_path parameter to specify the path to mp4edit)"
-    exit 1
+	Write-Host "mp4edit not found. (You can set the mp4edit_path parameter to specify the path to mp4edit)"
+	exit 1
 }
 
 if (-not $output_file) {
-    $output_file = "$($input_file.Substring(0, $input_file.LastIndexOf('.')))_processed.mp4"
+	$output_file = "$($input_file.Substring(0, $input_file.LastIndexOf('.')))_processed.mp4"
 }
 
 if (-not (Test-Path -Path $input_file -PathType Leaf)) {
-    Write-Host "Input file does not exist: $input_file"
-    exit 1
+	Write-Host "Input file does not exist: $input_file"
+	exit 1
 }
 
 if ($metadata_dir_path.EndsWith("\")) {
-    $metadata_dir_path = $metadata_dir_path.TrimEnd("\")
+	$metadata_dir_path = $metadata_dir_path.TrimEnd("\")
 }
 
 & $ffmpeg_path -i $input_file -s 3840x1920 -c:a copy remuxed.mp4
